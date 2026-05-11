2f703e12b0904c60bf73d1ea39aad4d505d491f6
snmg0at <snm.g0at@gmail.com>
2026-05-11 18:15:01 +0900

    Remove Cloudflare plugin from Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
-import { cloudflare } from "@cloudflare/vite-plugin";
 import { reactRouter } from "@react-router/dev/vite";
 
 export default defineConfig({
-  plugins: [reactRouter(), cloudflare()],
+  plugins: [reactRouter()],
 });
